8c6da155cf19996473451e4e68e6b7cf963e2bf7
Alex Hughes <aphughes@talk21.com>
2018-05-22 15:01:24 +0100

    Refactored mail controller code and created weekly-mail-model

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -8,6 +8,7 @@ class AreasController < ApplicationController
   def show
     @area = Area.find(params[:id])
     #only show area page to user(trainer) location
+    @weekly_email = WeeklyEmail.new
     if current_user.area.location == @area.location
       @area
     else redirect_to areas_path
@@ -16,42 +17,36 @@ class AreasController < ApplicationController
   end
 
   def mail
+    @weekly_email = WeeklyEmail.new(mail_params)
     #Grab content from form
-    main_content = params[:mail][:maincontent]
-    regular_title = params[:mail][:regulartitle]
-    regular_grouprun = params[:mail][:regulargrouprun]
-    regular_mission = params[:mail][:regularmission]
-    regular_coachrun = params[:mail][:regularcoachrun]
-    lapsed_title = params[:mail][:lapsedtitle]
-    lapsed_grouprun = params[:mail][:lapsedgrouprun]
-    lapsed_mission = params[:mail][:lapsedmission]
-    lapsed_coachrun = params[:mail][:lapsedcoachrun]
-    never_title = params[:mail][:nevertitle]
-    never_grouprun = params[:mail][:nevergrouprun]
-    never_mission = params[:mail][:nevermission]
-    never_coachrun = params[:mail][:nevercoachrun]
     runners = current_user.runners
     user = current_user
 
     #Loop over runners
     runners.each do |runner|
-      RunnerMailer.bespoke_email(user, runner, main_content,
-      regular_title,
-      regular_grouprun,
-      regular_mission,
-      regular_coachrun,
-      lapsed_title,
-      lapsed_grouprun,
-      lapsed_mission,
-      lapsed_coachrun,
-      never_title,
-      never_grouprun,
-      never_mission,
-      never_coachrun).deliver
+      RunnerMailer.bespoke_email(user, runner, @weekly_email).deliver
     end
 
     #Redirect user
     redirect_to areas_path
     flash[:alert] = "Emails sent!"
+  end
+
+  private
+
+  def mail_params
+    params.require(:weekly_email).permit(:main_content,
+      :regular_title,
+      :regular_grouprun,
+      :regular_mission,
+      :regular_coachrun,
+      :lapsed_title,
+      :lapsed_grouprun,
+      :lapsed_mission,
+      :lapsed_coachrun,
+      :never_title,
+      :never_grouprun,
+      :never_mission,
+      :never_coachrun)
   end
 end
